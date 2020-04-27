ce497876bec7f61d50d3a23a05a7b104cf2c8ad6
Ulrich Thiel <mail@ulthiel.com>
2020-04-27 18:15:14 +0200

    Added selfcheck-commit.php script

---
+++ Tools/Selfcheck/selfcheck-commit.php
@@ -0,0 +1,34 @@
+<?php
+/*
+   Copyright (C) 2020 Ulrich Thiel
+   https://github.com/ulthiel/magma-ut
+   thiel@mathematik.uni-kl.de, https://ulthiel.com/math
+
+   PHP script to add selfcheck report for Magma-UT.
+   Copy this script to your server.
+*/
+
+// Set MySQL database variables here
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$columns = implode(", ",array_keys($_GET));
+$count = count($_GET);
+$escaped_values = array_map('mysqli_real_escape_string', array_fill(1,$count,$conn), array_values($_GET));
+$values  = "\"" . implode("\", \"", $escaped_values) . "\"";
+$sql = "INSERT INTO selfchecks ($columns) VALUES ($values)";
+$result = $conn->query($sql);
+
+$conn->close();
+echo $sql;
+?>
