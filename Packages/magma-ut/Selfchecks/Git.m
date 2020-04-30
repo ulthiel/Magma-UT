3f8c1a29d36c008f7a89578c212ea927b6670a64
Ulrich Thiel <mail@ulthiel.com>
2020-04-30 17:37:29 +0200

    Fixed Git selfcheck

--- Packages/magma-ut/Selfchecks/Git.m
+++ Packages/magma-ut/Selfchecks/Git.m
@@ -2,6 +2,6 @@ dir := GetTempDir();
 GitCloneRemote("https://github.com/ulthiel/magma-ut-test-pkg", dir);
 dir := MakePath([dir, "magma-ut-test-pkg"]);
 V := GitRepositoryVersion(dir);
-DeleteFile(dir);
+DeleteDirectory(dir);
 IsGitInstalled();
 IsGitLFSInstalled();
