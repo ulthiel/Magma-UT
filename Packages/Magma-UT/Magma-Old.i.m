0a282dfdd02f2ffe6000eff635f74a5bd39933a5
Ulrich Thiel <thiel@mathematik.uni-kl.de>
2021-03-22 16:51:18 +0100

    Added Magma-Old with dummy implementations of ReadObject and WriteObject because these functions didn't exist before 2.25.

---
+++ Packages/Magma-UT/Magma-Old.i.m
@@ -0,0 +1,26 @@
+//##############################################################################
+//
+// Magma-UT
+// Copyright (C) 2020-2021 Ulrich Thiel
+// Licensed under GNU GPLv3, see License.md
+// https://github.com/ulthiel/magma-ut
+// thiel@mathematik.uni-kl.de, https://ulthiel.com/math
+//
+// This file will only be attached if the Magma version is < 2.25 to avoid
+// errors.
+//
+//##############################################################################
+
+intrinsic ReadObject(I::IO) -> .
+{}
+
+	error "Function ReadObject not implemented in Magma version < 2.25";
+
+end intrinsic;
+
+intrinsic WriteObject(I::IO, x::Any)
+{}
+
+	error "Function WriteObject not implemented in Magma version < 2.25";
+
+end intrinsic;
