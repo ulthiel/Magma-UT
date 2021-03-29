8f9862e2dc4b3a642c74361722323e032035d3d4
Ulrich Thiel <thiel@mathematik.uni-kl.de>
2021-03-29 09:58:13 +0200

    Added bzip2 option in compression.

--- Packages/Magma-UT/Sources/Compression.i.m
+++ Packages/Magma-UT/Sources/Compression.i.m
@@ -11,27 +11,40 @@ freeze;
 //
 //##############################################################################
 
-intrinsic WriteCompressed(F::MonStgElt, X::. : Level:=0)
-{Saves X compressed with gzip to the file F.}
+intrinsic WriteCompressed(F::MonStgElt, X::. : Level:=0, Type:="gzip")
+{Saves X compressed with to the file F. Type can be "gzip" (default) or "bzip2".}
 
 	//I tested with a 500MB string of random characters. The Pipe method
 	//is quicker than first writing to a file and then compress.
-	//Also gzip is much faster than bzip.
+	//Also gzip is much faster than bzip2.
+
+	require Type eq "gzip" or Type eq "bzip2": "Type needs to be either gzip or bzip2.";
+
+	tool := Type;
 
 	if Level ne 0 then
-		cmd := GetUnixTool("gzip")*" -"*Sprint(Level)*" > \""*F*"\"";
+		cmd := GetUnixTool(tool)*" -"*Sprint(Level)*" > \""*F*"\"";
 	else
-		cmd := GetUnixTool("gzip")*" > \""*F*"\"";
+		cmd := GetUnixTool(tool)*" > \""*F*"\"";
 	end if;
 	pipe := POpen(cmd, "w");
 	Write(pipe, X);
 
 end intrinsic;
 
-intrinsic ReadCompressed(F::MonStgElt) -> MonStgElt
-{Decompresses the gzip compressed file F and reads the data.}
+intrinsic ReadCompressed(F::MonStgElt : Type:="gzip") -> MonStgElt
+{Decompresses the compressed file F and reads the data. Type can be "gzip" (default) or "bzip2".}
 
-	cmd := GetUnixTool("gunzip")*" -c -d \""*F*"\"";
+	require Type eq "gzip" or Type eq "bzip2": "Type needs to be either gzip or bzip2.";
+
+	if Type eq "gzip" then
+		tool := "gunzip";
+	elif Type eq "bzip2" then
+		tool := "bunzip2";
+	end if;
+
+	cmd := GetUnixTool(tool)*" -c -d \""*F*"\"";
+
 	return SystemCall(cmd);
 
 end intrinsic;
